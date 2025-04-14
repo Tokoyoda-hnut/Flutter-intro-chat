b49c15950a43e8c64d598bbc637a724ecf5e457a
Tokoyoda-hnut <tokoyoda.hnut@gmail.com>
2025-04-14 15:01:55 +0900

    MOD: post一覧をリアルタイムに更新

--- chat_firebase_app/lib/main.dart
+++ chat_firebase_app/lib/main.dart
@@ -102,36 +102,62 @@ class _ChatPageState extends State<ChatPage> {
       appBar: AppBar(
         title: const Text('チャット'),
       ),
-      body: Center(
-        child: TextFormField(
-            onFieldSubmitted: (text) {
-            // まずは user という変数にログイン中のユーザーデータを格納します
-            final user = FirebaseAuth.instance.currentUser!;
+     body: Column(
+       children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot<Post>>(
+              // stream プロパティに snapshots() を与えると、コレクションの中のドキュメントをリアルタイムで監視することができます。
+              //stream: postsReference.snapshots(),
+              stream: postsReference.orderBy('createdAt').snapshots(),
+              // ここで受け取っている snapshot に stream で流れてきたデータが入っています。
+              builder: (context, snapshot) {
+                // docs には Collection に保存されたすべてのドキュメントが入ります。
+                // 取得までには時間がかかるのではじめは null が入っています。
+                // null の場合は空配列が代入されるようにしています。
+                final docs = snapshot.data?.docs ?? [];
+                return ListView.builder(
+                  itemCount: docs.length,
+                  itemBuilder: (context, index) {
+                    // data() に Post インスタンスが入っています。
+                    // これは withConverter を使ったことにより得られる恩恵です。
+                    // 何もしなければこのデータ型は Map になります。
+                    final post = docs[index].data();
+                    return Text(post.text);
+                  },
+                );
+              },
+            ),
+          ),
+         TextFormField(
+           onFieldSubmitted: (text) {
+             // まずは user という変数にログイン中のユーザーデータを格納します
+             final user = FirebaseAuth.instance.currentUser!;
 
-            final posterId = user.uid; // ログイン中のユーザーのIDがとれます
-            final posterName = user.displayName!; // Googleアカウントの名前がとれます
-            final posterImageUrl = user.photoURL!; // Googleアカウントのアイコンデータがとれます
+             final posterId = user.uid; // ログイン中のユーザーのIDがとれます
+             final posterName = user.displayName!; // Googleアカウントの名前がとれます
+             final posterImageUrl = user.photoURL!; // Googleアカウントのアイコンデータがとれます
 
-            // 先ほど作った postsReference からランダムなIDのドキュメントリファレンスを作成します
-            // doc の引数を空にするとランダムなIDが採番されます
-            final newDocumentReference = postsReference.doc();
+             // 先ほど作った postsReference からランダムなIDのドキュメントリファレンスを作成します
+             // doc の引数を空にするとランダムなIDが採番されます
+             final newDocumentReference = postsReference.doc();
 
-            final newPost = Post(
-              text: text,
-              createdAt: Timestamp.now(), // 投稿日時は現在とします
-              posterName: user.displayName!,
-              posterImageUrl: user.photoURL!,
-              posterId: user.uid,
-              reference: newDocumentReference,
-            );
+             final newPost = Post(
+               text: text,
+               createdAt: Timestamp.now(), // 投稿日時は現在とします
+               posterName: posterName,
+               posterImageUrl: posterImageUrl,
+               posterId: posterId,
+               reference: newDocumentReference,
+             );
 
-            // 先ほど作った newDocumentReference のset関数を実行するとそのドキュメントにデータが保存されます。
-            // 引数として Post インスタンスを渡します。
-            // 通常は Map しか受け付けませんが、withConverter を使用したことにより Post インスタンスを受け取れるようになります。
-            newDocumentReference.set(newPost);
-          },
-        ),
-      ),
-    );
-  }
+             // 先ほど作った newDocumentReference のset関数を実行するとそのドキュメントにデータが保存されます。
+             // 引数として Post インスタンスを渡します。
+             // 通常は Map しか受け付けませんが、withConverter を使用したことにより Post インスタンスを受け取れるようになります。
+             newDocumentReference.set(newPost);
+           },
+         ),
+       ],
+     ),
+   );
+ }
 }
