216a2b1e833e2bc1a797ba6a0210fdc5aa15c5be
Tokoyoda-hnut <tokoyoda.hnut@gmail.com>
2025-04-14 14:56:12 +0900

    MOD: postの保存

--- chat_firebase_app/lib/main.dart
+++ chat_firebase_app/lib/main.dart
@@ -103,7 +103,34 @@ class _ChatPageState extends State<ChatPage> {
         title: const Text('チャット'),
       ),
       body: Center(
-        child: TextFormField(),
+        child: TextFormField(
+            onFieldSubmitted: (text) {
+            // まずは user という変数にログイン中のユーザーデータを格納します
+            final user = FirebaseAuth.instance.currentUser!;
+
+            final posterId = user.uid; // ログイン中のユーザーのIDがとれます
+            final posterName = user.displayName!; // Googleアカウントの名前がとれます
+            final posterImageUrl = user.photoURL!; // Googleアカウントのアイコンデータがとれます
+
+            // 先ほど作った postsReference からランダムなIDのドキュメントリファレンスを作成します
+            // doc の引数を空にするとランダムなIDが採番されます
+            final newDocumentReference = postsReference.doc();
+
+            final newPost = Post(
+              text: text,
+              createdAt: Timestamp.now(), // 投稿日時は現在とします
+              posterName: user.displayName!,
+              posterImageUrl: user.photoURL!,
+              posterId: user.uid,
+              reference: newDocumentReference,
+            );
+
+            // 先ほど作った newDocumentReference のset関数を実行するとそのドキュメントにデータが保存されます。
+            // 引数として Post インスタンスを渡します。
+            // 通常は Map しか受け付けませんが、withConverter を使用したことにより Post インスタンスを受け取れるようになります。
+            newDocumentReference.set(newPost);
+          },
+        ),
       ),
     );
   }
